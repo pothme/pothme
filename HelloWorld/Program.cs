3afb6b54581e543dfa9f16199c5559dee5faccb6
pothme <89571975+pothme@users.noreply.github.com>
2022-03-07 11:37:23 +0900

    Revert "test33"
    This reverts commit d70ca014c0e0f1b784d4a90d1228a2a39c588424.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,7 +8,6 @@ namespace HelloWorld
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!22");
-            Console.WriteLine("Hello World!33");
         }
     }
 }
